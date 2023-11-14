a2dae887ce3816a8d13c2a0447ebd0fc4e6faa1a
equadras <emanueljose.quadras@gmail.com>
2023-11-14 15:30:43 -0300

    new folder

---
+++ goapi/cmd/api/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi"
+	"github.com/equadras/goapi/internal/handlers"
+	log "github.com/sirupsen/logrus"
+)
+
+
+func main(){
+
+	log.SetReportCaller(true)
+	var r *chi.Mux = chi.NewRouter()
+	handlers.Handler(r)
+
+	fmt.Println("Starting GO API service...")
+
+	fmt.Println("iniciando o serviço !")
+
+  err := http.ListenAndServe("localhost:8000", r)
+  if err != nil {
+	  log.Error(err)
+  }
+	
+}
